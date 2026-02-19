1d0e6c21f23103905b5ee53edab92a6d730c53ed
BHznJNs <441768875@qq.com>
2026-02-19 13:56:51 +0800

    SwitchField bug fixed

--- src-frontend/src/components/custom/form/fields/SwitchField.tsx
+++ src-frontend/src/components/custom/form/fields/SwitchField.tsx
@@ -1,20 +1,28 @@
-import { useFormContext } from "react-hook-form";
+import { useController, useFormContext } from "react-hook-form";
 import { FieldItem } from "@/components/custom/item/FieldItem";
 import { Switch } from "@/components/ui/switch";
 import type { FieldProps } from ".";
 
 type SwitchFieldProps = FieldProps<typeof Switch>;
 
-export function SwitchField({
-  fieldName,
-  fieldProps = { label: "启用" },
-  controlProps,
-}: SwitchFieldProps) {
-  const { register, getFieldState } = useFormContext();
+export function SwitchField({ fieldName, fieldProps = { label: "启用" }, controlProps }: SwitchFieldProps) {
+  const { control, getFieldState } = useFormContext<Record<string, boolean>>();
+  const { field } = useController({
+    name: fieldName,
+    control,
+  });
 
   return (
     <FieldItem {...fieldProps} fieldState={getFieldState(fieldName)}>
-      <Switch {...register(fieldName, { value: false })} {...controlProps} />
+      <Switch
+        checked={field.value}
+        onCheckedChange={field.onChange}
+        onBlur={field.onBlur}
+        name={field.name}
+        disabled={controlProps?.disabled}
+        required={controlProps?.required}
+        {...controlProps}
+      />
     </FieldItem>
   );
 }
