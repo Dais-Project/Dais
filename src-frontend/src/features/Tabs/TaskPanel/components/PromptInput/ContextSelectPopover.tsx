daa0dd7c5084e44773f8aff6c81f926a635cefc4
BHznJNs <441768875@qq.com>
2026-02-26 12:21:19 +0800

    file-tree and lazy-file-tree component

--- src-frontend/src/features/Tabs/TaskPanel/components/PromptInput/ContextSelectPopover.tsx
+++ src-frontend/src/features/Tabs/TaskPanel/components/PromptInput/ContextSelectPopover.tsx
@@ -1,25 +1,40 @@
 import { PromptInputButton } from "@/components/ai-elements/prompt-input";
+import { LazyFileTree } from "@/components/custom/LazyFileTree";
 import { Command, CommandEmpty, CommandInput, CommandItem, CommandList } from "@/components/ui/command";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
-import { AtSignIcon, GlobeIcon } from "lucide-react";
+import { AtSignIcon, FileIcon } from "lucide-react";
 import { useState } from "react";
 
 function FilesMenu({ onSelect }: { onSelect?: (path: string) => void }) {
+  const [search, setSearch] = useState("");
+  const isSearching = search.length > 0;
+
   return (
-    <Command>
+    <Command shouldFilter={isSearching ? undefined : false}>
       <CommandInput
         className="border-none focus-visible:ring-0"
         placeholder="Add files, folders, docs..."
+        value={search} onValueChange={setSearch}
       />
       <CommandList className="p-1">
         <CommandEmpty className="p-3 text-muted-foreground text-sm">
           No results found.
         </CommandEmpty>
 
-        <CommandItem >
-          <GlobeIcon className="text-primary" />
-          <span className="font-medium text-sm">Test.tsx</span>
-        </CommandItem>
+        {isSearching ? (
+          <CommandItem >
+            <FileIcon />
+            <span className="font-medium text-sm">Test.tsx</span>
+          </CommandItem>
+        ) : (
+          <LazyFileTree
+            rootNodes={[
+              // { path: "src", name: "src", type: "folder" },
+              // { path: "package.json", name: "package.json", type: "file" },
+            ]}
+            loadChildren={async () => []}
+          />
+        )}
       </CommandList>
     </Command>
   )
