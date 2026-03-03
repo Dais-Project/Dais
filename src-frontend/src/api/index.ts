576bfc37a8ceb3745a4d7682875a6c3fa798d39e
BHznJNs <441768875@qq.com>
2026-03-03 13:38:44 +0800

    prevents open devtools with keydown in production

--- src-frontend/src/api/index.ts
+++ src-frontend/src/api/index.ts
@@ -1,6 +1,6 @@
 const port = globalThis.__INJECTED__.server_port;
 export const API_BASE = `http://localhost:${port}/`;
 
-let resolveReady!: ()=>void
+let resolveReady!: () => void
 export const backendReady = new Promise<void>(r => resolveReady = r)
 export const setBackendReady = () => resolveReady()
