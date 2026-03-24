8b7e65f3ef0aaf502439da2ea6abbb282abf6907
BHznJNs <441768875@qq.com>
2026-03-24 21:59:28 +0800

    skill orm model

---
+++ src-server/src/db/models/skill.py
@@ -0,0 +1,24 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+from . import Base, relationship
+
+
+class SkillResource(Base):
+    __tablename__ = "skill_resources"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    relative: Mapped[str]
+    content: Mapped[str]
+    _skill_id: Mapped[int] = mapped_column(ForeignKey("skills.id"))
+    skill: Mapped[Skill] = relationship(back_populates="resources",
+                                        viewonly=True)
+
+class Skill(Base):
+    __tablename__ = "skills"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    description: Mapped[str] = mapped_column(default="")
+    is_enabled: Mapped[bool] = mapped_column(default=True)
+
+    content: Mapped[str]
+    resources: Mapped[list[SkillResource]] = relationship(back_populates="skill",
+                                                          cascade="all, delete-orphan")
