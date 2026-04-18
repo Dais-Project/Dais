f287ba9a9241c4a078f36d7ac6a1a7da2e6122a2
BHznJNs <441768875@qq.com>
2026-04-18 16:03:17 +0800

    improved save-file implementation

--- src-frontend/src/lib/save-file.ts
+++ src-frontend/src/lib/save-file.ts
@@ -1,19 +1,48 @@
 import { isTauri, saveFile as tauriSaveFile } from "./tauri";
 
-function dynamicDownloadFile(filename: string, content: string) {
-  const blob = new Blob([content], { type: "text/plain" });
+async function downloadFile(
+  filename: string,
+  content: string | ArrayBuffer | Blob,
+  mimeType: string = "text/plain"
+) {
+  const blob =
+    content instanceof Blob
+      ? content
+      : new Blob([content], { type: mimeType });
+
+  // prefer to use modern showSaveFilePicker
+  if ("showSaveFilePicker" in window) {
+    try {
+      const fileHandle = await window.showSaveFilePicker({
+        suggestedName: filename,
+      });
+      const writable = await fileHandle.createWritable();
+      await writable.write(blob);
+      await writable.close();
+      return;
+    } catch (e: any) {
+      if (e.name === "AbortError") {
+        // The user cancelled downloading
+        return;
+      }
+      console.error("Failed to download file:", e);
+    }
+  }
+
   const url = URL.createObjectURL(blob);
   const a = document.createElement("a");
   a.href = url;
   a.download = filename;
+  document.body.appendChild(a);
   a.click();
-  URL.revokeObjectURL(url);
+  document.body.removeChild(a);
+  setTimeout(() => URL.revokeObjectURL(url), 100);
 }
 
 export async function saveFile(filename: string, content: string) {
   if (isTauri) {
     await tauriSaveFile(filename, content);
   } else {
-    dynamicDownloadFile(filename, content);
+    await downloadFile(filename, content);
   }
 }
