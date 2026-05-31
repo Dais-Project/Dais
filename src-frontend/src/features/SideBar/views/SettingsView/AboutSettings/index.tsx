4622992274840802b35f43c7015bca9dd62e5ebf
BHznJNs <bhznjns@outlook.com>
2026-05-31 19:23:02 +0800

    feat: author item in the about page

--- src-frontend/src/features/SideBar/views/SettingsView/AboutSettings/index.tsx
+++ src-frontend/src/features/SideBar/views/SettingsView/AboutSettings/index.tsx
@@ -1,6 +1,7 @@
 import { useTranslation } from "react-i18next";
 import { SIDEBAR_NAMESPACE } from "@/i18n/resources";
 import { SettingItem } from "@/components/custom/item/SettingItem";
+import { LinkItem } from "@/components/custom/item/LinkItem";
 
 export function AboutSettings() {
   const { t } = useTranslation(SIDEBAR_NAMESPACE);
@@ -10,6 +11,10 @@ export function AboutSettings() {
     <div className="px-4 py-2">
       <SettingItem title={t("settings.about.version.title")}>
         <span className="text-sm text-muted-foreground">{version}</span>
+      </SettingItem>
+
+      <SettingItem title={t("settings.about.author.title")}>
+        <LinkItem href="https://github.com/BHznJNs">BHznJNs</LinkItem>
       </SettingItem>
     </div>
   );
